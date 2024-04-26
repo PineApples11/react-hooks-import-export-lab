57d33bc5e56b0ece93b081fa402c132be67a71bc
PineApples11 <kinyanjuiivy1@gmail.com>
2024-04-26 19:29:06 +0300

    import and export lab

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,9 @@
 const username = "Liza";
+export {username}
+
 const city = "New York";
+export{city}
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
-
+export{image}
 // export all three variables as named exports
